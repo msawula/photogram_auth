07bc694c6d24d446acaa9bbad4cc8c98823c5995
Michael Sawula <msawula@users.noreply.github.com>
2016-11-29 13:48:59 -0600

    Nearly done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,14 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, :keys => [:username])
 
-  devise_parameter_sanitizer.permit(:account_update, :keys => [:usernamel])
+  devise_parameter_sanitizer.permit(:account_update, :keys => [:username])
 end
 
 
