4020adf32374d903ab405efcf6d6a1cb1ddcaa9e
Michael Sawula <msawula@users.noreply.github.com>
2016-11-29 12:56:42 -0600

    changed photo code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, :keys => [:username])
+
+  devise_parameter_sanitizer.permit(:account_update, :keys => [:usernamel])
+end
+
+
 end
